8d86dbacefb1dda80d82cdac3d4a71f6551ef0fa
Andrimarciely <2019003790@ifam.edu.br>
2022-03-11 17:05:53 -0300

    Add textName

--- iTeleservice/ViewController.swift
+++ iTeleservice/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
